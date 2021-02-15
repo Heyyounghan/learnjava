499e92f89902408165f3f28a763d0018b1baa20a
heyyoung <bora_m13@naver.com>
2021-02-15 20:38:02 +0900

    210215

---
+++ study06/src/com/iu/weather/phone/PhonebookInfo.java
@@ -0,0 +1,16 @@
+package com.iu.weather.phone;
+
+public class PhonebookInfo {
+	
+	private String data;
+	
+	public PhonebookInfo() {
+		//이름, 전화번호 나이 메모
+		//version1 
+		this.data="혜니-01012341234-26-sing";
+		this.data=this.data+"예니-01045674567-25-marketing";
+		this.data+="지니-01011112222-24-student";
+		//version2= 
+	}
+
+}
---
+++ study06/src/com/iu/weather/phone/PhonebookMain.java
@@ -0,0 +1,14 @@
+package com.iu.weather.phone;
+
+public class PhonebookMain {
+
+	public static void main(String[] args) {
+		//1.phonebook을 초기화
+		//2.phonebook을 출력
+		//3.phonebook검색(이름)
+		//v2. 추가 삭제 기능 
+		//4. 종료
+
+	}
+
+}
